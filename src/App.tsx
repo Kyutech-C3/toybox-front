7acab0b94f6bc0bcbbcf976dd09c35d416fd3171
Semi-koron <6jimoto6@gmail.com>
2025-10-17 19:27:10 +0900

    feat auth edit user pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
+import AuthPage from "@/pages/AuthPage";
+import EditPage from "@/pages/EditPage";
 import TopPage from "@/pages/TopPage";
+import UserPage from "@/pages/UserPage";
+import WorkPage from "@/pages/WorkPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<TopPage />} />
+      <Route path="/auth" element={<AuthPage />} />
+      <Route path="/work/:id" element={<WorkPage />} />
+      <Route path="/user/:id" element={<UserPage />} />
+      <Route path="/edit/:id" element={<EditPage />} />
     </Routes>
   );
 }
