6456d765745a4c8e7c84c8f03e346fe9958a5c78
Semi-koron <6jimoto6@gmail.com>
2025-11-11 03:18:26 +0900

    remove extra store

--- src/features/WorkIndex/store/useWorksStore.ts
+++ src/features/WorkIndex/store/useWorksStore.ts
@@ -1,19 +1,13 @@
 import { create } from "zustand";
 
-import type { Work } from "@/shared/types/work";
-
 type WorksStoreProps = {
-  works: Work[];
   tags: string[];
-  setWorks: (works: Work[]) => void;
   addTag: (tag: string) => void;
   removeTag: (index: number) => void;
 };
 
 export const useWorksStore = create<WorksStoreProps>((set) => ({
-  works: [],
   tags: [],
-  setWorks: (works: Work[]) => set({ works }),
   addTag: (tag: string) => set((state) => ({ tags: [...state.tags, tag] })),
   removeTag: (index: number) =>
     set((state) => ({
