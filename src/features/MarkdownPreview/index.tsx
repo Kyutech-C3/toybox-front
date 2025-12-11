0eed5a28bc029d7befa44af0a74af3c4152f57cf
U5 <kyutech.joko@gmail.com>
2025-12-12 00:51:38 +0900

    update:コードブロックのデザインをプログラミング言語で変更#48

--- src/features/MarkdownPreview/index.tsx
+++ src/features/MarkdownPreview/index.tsx
@@ -1,4 +1,6 @@
 import Markdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { prism as style } from "react-syntax-highlighter/dist/esm/styles/prism";
 import rehypeRaw from "rehype-raw";
 import remarkBreaks from "remark-breaks";
 import remarkGfm from "remark-gfm";
@@ -15,6 +17,26 @@ const MarkdownPreview = ({ content }: MarkdownPreviewProps) => {
       <Markdown
         remarkPlugins={[remarkGfm, remarkBreaks]}
         rehypePlugins={[rehypeRaw]}
+        components={{
+          code(props) {
+            const { children, className, node, ref, ...rest } = props;
+            const match = /language-(\w+)/.exec(className || "");
+            return match ? (
+              <SyntaxHighlighter
+                {...rest}
+                PreTag="div"
+                language={match[1]}
+                style={style}
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <code {...rest} className={className}>
+                {children}
+              </code>
+            );
+          },
+        }}
       >
         {content}
       </Markdown>
