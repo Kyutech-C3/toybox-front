24a1dcd73e6debdb4cee431c46d584e800f26d41
Semi-koron <6jimoto6@gmail.com>
2025-12-13 07:07:17 +0900

    fix header and work post method

--- src/features/WorkEditor/api/postWork.ts
+++ src/features/WorkEditor/api/postWork.ts
@@ -3,12 +3,17 @@ import { postDataWithAuth } from "@/util/fetchData";
 import type { WorkRequestData } from "@/shared/types/work";
 
 const postWork = async (data: WorkRequestData, accessToken: string) => {
-  const response = await postDataWithAuth(
-    "/auth/works",
-    JSON.stringify(data),
-    accessToken,
-  );
-  return response;
+  try {
+    const response = await postDataWithAuth(
+      "/auth/works",
+      JSON.stringify(data),
+      accessToken,
+    );
+    return response;
+  } catch (error) {
+    console.error("Error posting work:", error);
+    return null;
+  }
 };
 
 export { postWork };
